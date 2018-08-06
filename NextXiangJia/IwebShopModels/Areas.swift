a7ad7aa7e043cb3cc1af53dc2efa5f1eb9f074de
yzc <756988824@qq.com>
2018-08-06 10:14:00 +0800

    添加地区信息类

---
+++ NextXiangJia/IwebShopModels/Areas.swift
@@ -0,0 +1,16 @@
+//
+//  Areas.swift
+//  NextXiangJia
+//  地区信息类
+//  Created by DEV2018 on 2018/8/6.
+//  Copyright © 2018年 DEV2018. All rights reserved.
+//
+
+import Foundation
+
+class Areas:BaseModel {
+    @objc var area_id:String = ""//地区id
+    @objc var parent_id:Int = -1//上一级的id值
+    @objc var area_name:String = ""//地区名称
+    @objc var sort:String = ""//排序 = 地区id
+}
